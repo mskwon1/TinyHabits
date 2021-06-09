61ddecb99c17d38bc172e743d450cb5417c98a8b
mskwon1 <mageeeeek@gmail.com>
2021-06-09 23:33:26 +0900

    apply tailwind css

--- tiny-habits-web/pages/index.tsx
+++ tiny-habits-web/pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Home(): JSX.Element {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="container mx-auto my-auto h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="text-gray-500 text-5xl text-center bg-purple-200 px-1">
+          Hello World
+        </div>
+      </div>
+    </div>
+  );
 }
