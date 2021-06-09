b2081566f01bcbc791d3c5daebe2d95dcbe12a7b
mskwon1 <mageeeeek@gmail.com>
2021-06-09 23:05:21 +0900

    add eslint & prettier

---
+++ tiny-habits-web/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+  },
+};
