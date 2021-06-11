b4b915f038ce21471c73cfbfa84c07ce1fe5ca60
MinSu Kwon <mageeeeek@gmail.com>
2021-06-12 04:40:15 +0900

    remove redux and add swr

--- tiny-habits-web/pages/_app.tsx
+++ tiny-habits-web/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { AppProps } from 'next/app';
-import wrapper from '../store';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
--- tiny-habits-web/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import count from './count';
-
-const rootReducer = combineReducers({
-  count,
-});
-
-export default rootReducer;
