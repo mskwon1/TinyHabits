3409a7434f36c1aba7912ea225237601cef1bd9a
mskwon1 <mageeeeek@gmail.com>
2021-10-11 20:20:48 +0900

    Implement temp navbar

--- tiny-habits-web/components/layout/NavigationBar.tsx
+++ tiny-habits-web/components/layout/NavigationBar.tsx
@@ -1,13 +1,72 @@
-import { AppBar, Toolbar, Button, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+import StarIcon from '@material-ui/icons/Star';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Link from 'next/link';
+
+const LoginButton = (): JSX.Element => {
+  return (
+    <Button variant="contained" color="primary" disableElevation={true}>
+      로그인
+    </Button>
+  );
+};
+
+type NavigationMenuProps = {
+  title: string;
+  href?: string;
+  className?: string;
+  icon?: JSX.Element;
+};
+
+const NavigationMenuButton = ({
+  icon,
+  title,
+  className,
+  href,
+}: NavigationMenuProps): JSX.Element => {
+  const buttonComponent = (
+    <Button className={`focus:outline-none ${className}`} startIcon={icon}>
+      {title}
+    </Button>
+  );
+
+  if (href) {
+    return <Link href={href}>{buttonComponent}</Link>;
+  }
+
+  return buttonComponent;
+};
+
+const NavigationMenus = (): JSX.Element => {
+  return (
+    <>
+      <NavigationMenuButton
+        title="습관 버리기"
+        href="/remove-habits"
+        icon={<DeleteIcon style={{ color: 'black' }} />}
+      />
+      <NavigationMenuButton
+        title="황금행동 찾기"
+        href="/golden-habits"
+        icon={<StarIcon style={{ color: 'gold' }} />}
+      />
+      {/* <NavigationMenu title="메뉴 3" /> */}
+      {/* <NavigationMenu title="메뉴 4" /> */}
+    </>
+  );
+};
 
 export function NavigationBar(): JSX.Element {
   return (
-    <AppBar position="sticky" color="secondary">
-      <Toolbar className="flex">
-        <div className="flex-grow px-3 text-xl">'습'</div>
-        <Button variant="contained" color="primary" disableElevation={true}>
-          로그인
-        </Button>
+    <AppBar className="px-3" position="sticky" color="secondary">
+      <Toolbar className="flex justify-between">
+        <Link href="/">
+          <div className="px-3 text-xl font-bold cursor-pointer">'습'</div>
+        </Link>
+        <div className="flex flex-grow justify-start space-x-2 mx-5">
+          <NavigationMenus />
+        </div>
+        <LoginButton />
       </Toolbar>
     </AppBar>
   );
