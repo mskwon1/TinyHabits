a96a65833cbf1ae771d41aeab52db9f1a22cd22c
mskwon1 <mageeeeek@gmail.com>
2022-01-03 23:18:13 +0900

    Adds DEFAULT_PORT_NUMBER const

--- tiny-habits-server/src/main.ts
+++ tiny-habits-server/src/main.ts
@@ -3,13 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+const DEFAULT_PORT_NUMBER = 3005;
+
 async function bootstrap() {
   const expressApp = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
   const configService = expressApp.get(ConfigService);
-  const port = +configService.get('PORT') || 3005;
+  const port = +configService.get('PORT') || DEFAULT_PORT_NUMBER;
 
   await expressApp.listen(port);
 }
