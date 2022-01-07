44b9f22934e0d67b4606e717d0eb3d61d68a87da
mskwon1 <mageeeeek@gmail.com>
2022-01-07 23:13:26 +0900

    Adds test module e2e spec file

---
+++ tiny-habits-server/src/api/test/test.e2e.spec.ts
@@ -0,0 +1,21 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { TestModule } from './test.module';
+
+describe('TestController', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TestModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET test', () => {
+    return request(app.getHttpServer()).get('/api/test').expect(HttpStatus.OK);
+  });
+});
