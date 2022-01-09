e8d16ab84ef71448f93ec656b85c31d6bd5a33a1
mskwon1 <mageeeeek@gmail.com>
2022-01-09 23:52:35 +0900

    Registers database module to main app module

--- tiny-habits-server/src/app.module.ts
+++ tiny-habits-server/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ApiModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ApiModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
