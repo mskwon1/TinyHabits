556add15406f7650b39704a4635d68c2f4fed4db
mskwon1 <mageeeeek@gmail.com>
2022-01-16 15:38:15 +0900

    Moves typeorm config

--- tiny-habits-server/src/app.module.ts
+++ tiny-habits-server/src/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
     ApiModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntitities: true,
+        }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
