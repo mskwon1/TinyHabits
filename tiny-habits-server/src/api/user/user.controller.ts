0a4ba5a0f60f022fd7c0394081833672da41f85b
mskwon1 <mageeeeek@gmail.com>
2022-01-23 01:03:39 +0900

    Implements create user controller method

--- tiny-habits-server/src/api/user/user.controller.ts
+++ tiny-habits-server/src/api/user/user.controller.ts
@@ -1,20 +1,15 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put } from '@nestjs/common';
+import { User } from '@src/models/users/user.entity';
+import { UserService } from '@src/models/users/user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('api/users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post('/')
-  async create() {
-    // TODO
-  }
-
-  @Get('/')
-  async findAll() {
-    // TODO
-  }
-
-  @Get('/:userId')
-  async findOne() {
-    // TODO
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = await this.userService.create(createUserDto);
+    return createdUser;
   }
 
   @Put('/:userId')
