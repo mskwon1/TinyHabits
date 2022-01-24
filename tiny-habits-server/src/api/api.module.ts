747d5598ae25087399d42f093d2468c48aca320e
mskwon1 <mageeeeek@gmail.com>
2022-01-25 00:46:22 +0900

    Registers AuthApiModule to api module

--- tiny-habits-server/src/api/api.module.ts
+++ tiny-habits-server/src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthApiModule } from './auth/auth-api.module';
 import { TestModule } from './test/test.module';
 import { UserApiModule } from './user/user-api.module';
 
 @Module({
-  imports: [TestModule, UserApiModule],
+  imports: [TestModule, UserApiModule, AuthApiModule],
 })
 export class ApiModule {}
