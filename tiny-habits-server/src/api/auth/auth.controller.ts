6a745f2b8dfa4a500bf063530171f030cdf72f86
mskwon1 <mageeeeek@gmail.com>
2022-01-25 00:59:00 +0900

    Implements login logic

--- tiny-habits-server/src/api/auth/auth.controller.ts
+++ tiny-habits-server/src/api/auth/auth.controller.ts
@@ -1,11 +1,24 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from '@src/models/users/user.service';
 import { LoginDto } from './dto/login.dto';
+import * as bcrpyt from 'bcrypt';
 
 @Controller('api/auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
   @Post('/login')
   async login(@Body() loginDto: LoginDto) {
-    // TODO : 구현
+    const { email, password } = loginDto;
+
+    const { password: hashPassword } = await this.userService.findOneByEmail(
+      email,
+      true,
+    );
+
+    const result = await bcrpyt.compare(password, hashPassword);
+    console.log(result);
+
     return true;
   }
 }
