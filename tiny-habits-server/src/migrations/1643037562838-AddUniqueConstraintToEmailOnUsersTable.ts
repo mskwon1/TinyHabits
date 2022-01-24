58dacd2246d474e13cf0fa12df18a56e7d1f294a
mskwon1 <mageeeeek@gmail.com>
2022-01-25 00:35:14 +0900

    Applies formating to migration file

--- tiny-habits-server/src/migrations/1643037562838-AddUniqueConstraintToEmailOnUsersTable.ts
+++ tiny-habits-server/src/migrations/1643037562838-AddUniqueConstraintToEmailOnUsersTable.ts
@@ -1,33 +1,34 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddUniqueConstraintToEmailOnUsersTable1643037562838 implements MigrationInterface {
-    name = 'AddUniqueConstraintToEmailOnUsersTable1643037562838'
+export class AddUniqueConstraintToEmailOnUsersTable1643037562838
+  implements MigrationInterface
+{
+  name = 'AddUniqueConstraintToEmailOnUsersTable1643037562838';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE \`user\` DROP COLUMN \`password\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user\`
             ADD \`password\` varchar(64) NOT NULL
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user\`
             ADD UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`)
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE \`user\` DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user\` DROP COLUMN \`password\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user\`
             ADD \`password\` varchar(64) NOT NULL
         `);
-    }
-
+  }
 }
