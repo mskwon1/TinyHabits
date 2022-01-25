34481ad3b29cdd3b1798394391d15dc59b49b1ec
mskwon1 <mageeeeek@gmail.com>
2022-01-25 23:28:43 +0900

    Registers local strategy to auth module

--- tiny-habits-server/src/api/auth/auth-api.module.ts
+++ tiny-habits-server/src/api/auth/auth-api.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '@src/models/users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthApiModule {}
