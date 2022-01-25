0b4e296a99e29224e9162c102f067c0b922a1116
mskwon1 <mageeeeek@gmail.com>
2022-01-26 00:26:24 +0900

    Adds sign jwt token method to auth service

--- tiny-habits-server/src/api/auth/auth.service.ts
+++ tiny-habits-server/src/api/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { User } from '@src/models/users/user.entity';
 import { UserService } from '@src/models/users/user.service';
 import _ from 'lodash';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<User | null> {
     const user = await this.usersService.findOneByEmail(email, true);
@@ -22,5 +26,12 @@ export class AuthService {
     }
 
     return _.omit(user, ['password']);
+  }
+
+  async signJwtToken(user: User): Promise<{ accessToken: string }> {
+    const { id, email } = user;
+    const payload = { email, sub: id };
+
+    return { accessToken: this.jwtService.sign(payload) };
   }
 }
