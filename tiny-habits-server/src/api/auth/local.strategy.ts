13cec22b05ed8bf73d735d51a7805101ef288ccb
mskwon1 <mageeeeek@gmail.com>
2022-01-25 23:28:18 +0900

    Implements `local` auth strategy

---
+++ tiny-habits-server/src/api/auth/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import _ from 'lodash';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+
+    if (_.isNil(user)) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
