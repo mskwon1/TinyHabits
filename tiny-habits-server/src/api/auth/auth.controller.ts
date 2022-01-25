4276d85db3fe779495a11da2d325033a23dc1827
mskwon1 <mageeeeek@gmail.com>
2022-01-26 00:26:59 +0900

    Adds access token to auth/login route return type

--- tiny-habits-server/src/api/auth/auth.controller.ts
+++ tiny-habits-server/src/api/auth/auth.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { UserService } from '@src/models/users/user.service';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Request() req) {
-    return req.user;
+  async login(
+    @Request() req,
+  ): Promise<{ userId: number; email: string; accessToken: string }> {
+    const { accessToken } = await this.authService.signJwtToken(req.user);
+    const { id, email } = req.user;
+
+    return {
+      userId: id,
+      email,
+      accessToken,
+    };
   }
 }
