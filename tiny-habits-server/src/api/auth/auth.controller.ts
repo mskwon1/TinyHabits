63daef7c2b5901151131c4e7eb38d721e29968ac
mskwon1 <mageeeeek@gmail.com>
2022-01-26 00:11:31 +0900

    Applies LocalAuthGuard decorator to login route

--- tiny-habits-server/src/api/auth/auth.controller.ts
+++ tiny-habits-server/src/api/auth/auth.controller.ts
@@ -1,24 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { UserService } from '@src/models/users/user.service';
-import { LoginDto } from './dto/login.dto';
-import * as bcrpyt from 'bcrypt';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Body() loginDto: LoginDto) {
-    const { email, password } = loginDto;
-
-    const { password: hashPassword } = await this.userService.findOneByEmail(
-      email,
-      true,
-    );
-
-    const result = await bcrpyt.compare(password, hashPassword);
-    console.log(result);
-
-    return true;
+  async login(@Request() req) {
+    return req.user;
   }
 }
