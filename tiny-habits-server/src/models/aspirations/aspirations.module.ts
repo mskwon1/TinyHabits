9b506fe3f02476cbce6c8decc8db343f2104d996
mskwon1 <mageeeeek@gmail.com>
2022-02-02 22:07:09 +0900

    Registers aspiration service to aspiration module

--- tiny-habits-server/src/models/aspirations/aspirations.module.ts
+++ tiny-habits-server/src/models/aspirations/aspirations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Aspiration } from './aspiration.entity';
+import { AspirationService } from './aspiration.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Aspiration])],
+  providers: [AspirationService],
+  exports: [AspirationService],
 })
 export class AspirationModule {}
