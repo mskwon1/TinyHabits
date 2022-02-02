89112b2a2b66b4fc89083c91161c06b913327fd9
mskwon1 <mageeeeek@gmail.com>
2022-02-02 22:12:38 +0900

    Implements aspiration service

--- tiny-habits-server/src/models/aspirations/aspiration.service.ts
+++ tiny-habits-server/src/models/aspirations/aspiration.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 import { Aspiration } from './aspiration.entity';
 
 @Injectable()
@@ -10,7 +11,20 @@ export class AspirationService {
     private aspirationRepository: Repository<Aspiration>,
   ) {}
 
-  async create(params): Promise<Aspiration> {}
+  async create(params: CreateAspirationParams): Promise<Aspiration> {
+    const { userId, name } = params;
+
+    const targetUser = new User();
+    targetUser.id = userId;
+
+    const aspiration = this.aspirationRepository.create({
+      user: targetUser,
+      name,
+    });
+    const createdAspiration = await this.aspirationRepository.save(aspiration);
+
+    return createdAspiration;
+  }
 
   async findAll(): Promise<Aspiration[]> {
     return this.aspirationRepository.find();
@@ -20,7 +34,19 @@ export class AspirationService {
     return this.aspirationRepository.findOne(aspirationId);
   }
 
-  async update(aspirationId: number, params): Promise<Aspiration> {}
+  async update(
+    aspirationId: number,
+    params: UpdateAspirationParams,
+  ): Promise<Aspiration> {
+    const { name } = params;
+
+    await this.aspirationRepository.update(aspirationId, { name });
+    const updatedAspiration = await this.aspirationRepository.findOne(
+      aspirationId,
+    );
+
+    return updatedAspiration;
+  }
 
   async delete(aspirationId: number): Promise<void> {
     await this.aspirationRepository.delete(aspirationId);
