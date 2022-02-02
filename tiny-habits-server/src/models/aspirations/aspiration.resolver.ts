7d2eaed8415eb3be902de6e8de1fc04379a9294b
mskwon1 <mageeeeek@gmail.com>
2022-02-02 23:19:08 +0900

    Adds user field resolver to aspiration resolver

--- tiny-habits-server/src/models/aspirations/aspiration.resolver.ts
+++ tiny-habits-server/src/models/aspirations/aspiration.resolver.ts
@@ -9,15 +9,27 @@ import {
 } from '@nestjs/graphql';
 import { GqlAuthGuard } from '@src/api/auth/gql-auth.guard';
 import { GqlUser } from '@src/decorators/gql-user.decorator';
+import { UserService } from '../users/user.service';
+import { Aspiration } from './aspiration.entity';
 import { AspirationModel } from './aspiration.model';
 import { AspirationService } from './aspiration.service';
 
 @Resolver((of) => AspirationModel)
 export class AspirationResolver {
-  constructor(private readonly aspirationService: AspirationService) {}
+  constructor(
+    private readonly aspirationService: AspirationService,
+    private readonly userService: UserService,
+  ) {}
 
   @Query((returns) => AspirationModel)
   async aspiration(@Args('id', { type: () => Int }) aspirationId: number) {
     return this.aspirationService.findOneById(aspirationId);
+  }
+
+  @ResolveField()
+  async user(@Parent() aspiration: Aspiration) {
+    const { userId } = aspiration;
+
+    return this.userService.findOneById(userId);
   }
 }
