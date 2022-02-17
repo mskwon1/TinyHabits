c29aa13012634e01d5e2ec0221e6bca628545b1a
mskwon1 <mageeeeek@gmail.com>
2022-02-17 23:23:42 +0900

    Applies form style

--- tiny-habits-web/pages/signup/index.tsx
+++ tiny-habits-web/pages/signup/index.tsx
@@ -3,18 +3,42 @@ import { Box, Container, TextField, Typography } from '@mui/material';
 const SignupPage: React.FC = () => {
   return (
     <Container>
-      <Box component="form">
-        <Typography variant="h5" component="h1" className="text-center">
+      <Box component="form" sx={{ paddingTop: 5 }}>
+        <Typography variant="h5" component="h1" sx={{ textAlign: 'center' }}>
           회원가입
         </Typography>
-        <TextField id="name" label="이름" variant="outlined" className="mt-3" />
-        <TextField
-          id="email"
-          label="이메일"
-          variant="outlined"
-          className="mt-3"
-        />
-        <TextField id="password" label="패스워드" variant="outlined" />
+        <Container
+          maxWidth="sm"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 8,
+          }}
+        >
+          <TextField
+            id="name"
+            label="이름"
+            variant="outlined"
+            fullWidth
+            sx={{ marginTop: 5 }}
+          />
+          <TextField
+            id="email"
+            label="이메일"
+            variant="outlined"
+            fullWidth
+            sx={{ marginTop: 5 }}
+          />
+          <TextField
+            id="password"
+            label="패스워드"
+            variant="outlined"
+            fullWidth
+            sx={{ marginTop: 5 }}
+          />
+        </Container>
       </Box>
     </Container>
   );
