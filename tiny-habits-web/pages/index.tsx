807cd9c777c8ea85702202ab15a2e1213251aca2
mskwon1 <mageeeeek@gmail.com>
2022-02-24 00:56:19 +0900

    Adds sample use session code

--- tiny-habits-web/pages/index.tsx
+++ tiny-habits-web/pages/index.tsx
@@ -1,3 +1,12 @@
+import { useSession } from 'next-auth/react';
+
 export default function Home(): JSX.Element {
-  return <div>Hello World</div>;
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div>Hello World</div>
+      {session && <div>LOGGED IN : {session.user.email}</div>}
+    </>
+  );
 }
